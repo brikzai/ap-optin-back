90102fec06e8fab00535b40d72b187109063323e
rdeli <powersmvp6@gmail.com>
2026-08-26 21:14:20 -0300

    feat: colunas cliente.status e cliente.atualizado_em

--- docker/initdb/00-cliente.sql
+++ docker/initdb/00-cliente.sql
@@ -5,6 +5,8 @@ CREATE TABLE cliente (
   nome           TEXT NOT NULL,
   email          TEXT,
   telefone       TEXT,
+  status         TEXT NOT NULL DEFAULT 'pending',
   criado_em      TIMESTAMPTZ NOT NULL DEFAULT now(),
+  atualizado_em  TIMESTAMPTZ NOT NULL DEFAULT now(),
   UNIQUE (documento)
 );
